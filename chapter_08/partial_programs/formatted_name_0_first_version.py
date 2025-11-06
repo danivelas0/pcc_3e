0196a974962f11090b79f9eb3759f0d9849f03f6
narjodan <deantoniovelasquez@gmail.com>
2025-11-06 16:49:59 -0500

    Functions #2

--- chapter_08/partial_programs/formatted_name_0_first_version.py
+++ chapter_08/partial_programs/formatted_name_0_first_version.py
@@ -1,7 +1,9 @@
 def get_formatted_name(first_name, last_name):
     """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
+    full_name = f"{first_name} {last_name}"  # what is going to return?
     return full_name.title()
 
+
+# where to store the returned value?
 musician = get_formatted_name('jimi', 'hendrix')
 print(musician)
--- chapter_08/partial_programs/tempCodeRunnerFile.py
+++ chapter_08/partial_programs/tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
-def describe_pet(animal_type, pet_name):
-    """Display information about a pet."""
-    print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s name is {pet_name.title()}.")
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-describe_pet('hamster', 'harry')
-describe_pet('dog','tobias')
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
